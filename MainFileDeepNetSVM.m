5618c6c222e4e5491caca30f848df9d12d11037f
Leon Palafox <leonoel49@gmail.com>
2015-01-21 13:35:37 -0700

    First File

---
+++ MainFileDeepNetSVM.m
@@ -0,0 +1,39 @@
+
+%%
+%%This script generates two datasets, one is a dataset with positive
+%%samples of the feature to classify, and another are negative samples.
+%%this is a two class classifier
+
+%First, we need to call a dialog to generate the settings file
+%TODO generate GUI
+%Here we do all by ourselves
+delete '*.mat'
+config.labels = {'Data Folder', 'File to use', 'Features to classify', 'Positive Examples', 'Negative Examples', 'Hidden Neurons', 'Minimum image size'};
+config.data{1} = 'C:\Users\leon\Documents\Data\GeneralData\ConeFields\';
+config.data{2} = 'ESP_018747_2065_RED.QLOOK.JP2';
+config.data{3} = 'cones';
+config.data{4} = 50;%positive examples
+config.data{5} = 100; %negative examples
+config.data{6} = 50; %hidden neurons
+config.data{7} = 40; %minimum size
+
+%Now that we have all the values, store all of them in an structure and
+%save them in our config file
+%
+create_config_file(config, 'config.lpd');% For the moment, the config file
+%is only to see in a text file all the variables
+%crop_working_image(config)
+
+create_dataset('cones', config)
+create_dataset('negative', config)
+[images, labels] = read_reshape_dataset_labeled(config);
+c = cvpartition(labels,'k',2); %create two a training and a testing dataset
+test_x = images(c.test(1),:);
+train_x = images(c.training(1),:);
+%run_stacked_autoencoder(images, labels, config)
+%run_autoencoder(zscore(images(1:20,:)), config)
+num_patches = 1000;
+[patches, upper_x, upper_y] = generate_random_patches(config, num_patches);
+%classify_patches %This scripts classifies the patches with the autoencoder
+%classify_svm
+%classifyCNN
