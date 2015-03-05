1ee42c9ca05945d5c7a2a0f80712027c9c3cdcb0
bojualvarez <bojualvarez@gmail.com>
2015-03-05 16:56:41 -0700

    Final LPSC Changes
    Pre-allocation for efficiency of HoG, addition of png images for running
    easier, tuned, cross-validated classifier, and changes in main file to
    match

--- MainFileDeepNetSVM.m
+++ MainFileDeepNetSVM.m
@@ -33,12 +33,12 @@ create_config_file(config, 'config.lpd');% For the moment, the config file
 %generate_windows('negative', config)
 %generate_windows('crater', config)
 
-[images, labels, features, image_structure] = read_reshape_dataset_labeled(config, 60);
-break
+image_structure = read_reshape_dataset_labeled(config, 20);
+
 %run_stacked_autoencoder(images, labels, config)
 %run_autoencoder(zscore(images(1:20,:)), config)
 num_patches = 10000;
-[patches, upper_x, upper_y] = generate_random_patches(config, num_patches);
+patches_structure = generate_random_patches(config, num_patches);
 %classify_patches %This scripts classifies the patches with the autoencoder
-classified=classify_svm_v3(image_structure);
+[Accuracy, pred_map, score_map]=classify_svm_v3(image_structure,patches_structure);
 classifyCNN
--- generate_random_patches.m
+++ generate_random_patches.m
@@ -1,4 +1,4 @@
-function [patches, upper_x, upper_y, patches_structure] = generate_random_patches(config, num_patches)
+function patches_structure = generate_random_patches(config, num_patches)
 %%
 %This function generates a set of random patches, while keeping track of
 %their coordinates
@@ -11,7 +11,7 @@ filename = [folder filename];
 hirise_img = imread(filename);
 hirise_img = double(hirise_img)/255;
 patch_size = config.data{7}; %Size of each patch
-patch_size = 60; %Size of each patch
+patch_size = 20; %Size of each patch
 [max_y, max_x] = size(hirise_img);
 upper_x = unidrnd(max_x-patch_size, 1, num_patches);
 upper_y = unidrnd(max_y-patch_size, 1, num_patches);
@@ -19,19 +19,31 @@ patches_array{2}=[];
 patches_array{4}=[];
 patches_array{6}=[];
 patches_array{8}=[];
+[patches_rowsize, patches_colsize]=size(hirise_img(upper_y(num_patches):upper_y(num_patches)+patch_size-1,upper_x(num_patches):upper_x(num_patches)+patch_size-1));
+patches_3dsize=num_patches;
+patches=zeros(patches_rowsize,patches_colsize,patches_3dsize);
 for patch_idx = 1:num_patches
     patches(:,:, patch_idx) = hirise_img(upper_y(patch_idx):upper_y(patch_idx)+patch_size-1, upper_x(patch_idx):upper_x(patch_idx)+patch_size-1);
-    for cell_size=2:2:8
-        patches_HoG(1,:,patch_idx)={extractHOGFeatures(patches(:,:,patch_idx),'CellSize',[cell_size cell_size])}; 
-        patches_array{cell_size}=[patches_array{cell_size} patches_HoG(1,:,patch_idx)];
-        clear patches_HoG
-    end    
+    temp2=extractHOGFeatures(patches(:,:,patch_idx),'CellSize',[2 2]);
+    temp4=extractHOGFeatures(patches(:,:,patch_idx),'CellSize',[4 4]);
+    temp8=extractHOGFeatures(patches(:,:,patch_idx),'CellSize',[8 8]);    
+    if patch_idx==1
+       patches_HoG2=zeros(num_patches,size(temp2,2));
+       patches_HoG4=zeros(num_patches,size(temp4,2));
+       patches_HoG8=zeros(num_patches,size(temp8,2));
+       patches_HoG2(patch_idx,:)=temp2;
+       patches_HoG4(patch_idx,:)=temp4;
+       patches_HoG8(patch_idx,:)=temp8;
+    else
+       patches_HoG2(patch_idx,:)=temp2;
+       patches_HoG4(patch_idx,:)=temp4;
+       patches_HoG8(patch_idx,:)=temp8;
+    end
+end
+
 [n, m, samples] = size(patches);
 patches = reshape(permute(patches,[3 2 1]),samples,n*m);
 patches = zscore(patches);
-patches_PCA={pca(patches')};
-patches_structure=struct('Patches', {patches}, 'HoG_Patches_2x2', {patches_array{2}}, 'HoG_Patches_4x4', {patches_array{4}}, 'HoG_Patches_6x6', {patches_array{6}},'HoG_Patches_8x8', {patches_array{8}}, 'Patches_PCA', {patches_PCA}); 
+patches_PCA=pca(patches');
+patches_structure=struct('Patches', {patches}, 'HoG_Patches_2x2', patches_HoG2, 'HoG_Patches_4x4', patches_HoG4,'HoG_Patches_8x8', patches_HoG8, 'Patches_PCA', patches_PCA, 'Upper_X',{upper_x},'Upper_Y', {upper_y}); 
 end
-end
-
-
