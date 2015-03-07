8534b8f7e2efc38271208223f11e50ae269e2714
Leon Palafox <leonoel49@gmail.com>
2015-03-07 13:20:22 -0700

    More Changes

--- generate_random_patches.m
+++ generate_random_patches.m
@@ -1,4 +1,8 @@
+<<<<<<< HEAD
 function [patches, upper_x, upper_y, patches_structure] = generate_random_patches(config, num_patches, pixel_size)
+=======
+function patches_structure = generate_random_patches(config, num_patches)
+>>>>>>> origin/master
 %%
 %This function generates a set of random patches, while keeping track of
 %their coordinates
@@ -10,7 +14,12 @@ filename = strcat(strfilename, '.png');
 filename = [folder filename];
 hirise_img = imread(filename);
 hirise_img = double(hirise_img)/255;
+<<<<<<< HEAD
 patch_size = pixel_size; %Size of each patch
+=======
+patch_size = config.data{7}; %Size of each patch
+patch_size = 20; %Size of each patch
+>>>>>>> origin/master
 [max_y, max_x] = size(hirise_img);
 upper_x = unidrnd(max_x-patch_size, 1, num_patches);
 upper_y = unidrnd(max_y-patch_size, 1, num_patches);
@@ -18,6 +27,7 @@ patches_array{2}=[];
 patches_array{4}=[];
 patches_array{6}=[];
 patches_array{8}=[];
+<<<<<<< HEAD
 patches = zeros(patch_size, patch_size, num_patches);
 
 for patch_idx = 1:num_patches
@@ -37,3 +47,33 @@ patches_structure=struct('Patches', {patches}, 'HoG_Patches_2x2', {patches_array
 end
 
 
+=======
+[patches_rowsize, patches_colsize]=size(hirise_img(upper_y(num_patches):upper_y(num_patches)+patch_size-1,upper_x(num_patches):upper_x(num_patches)+patch_size-1));
+patches_3dsize=num_patches;
+patches=zeros(patches_rowsize,patches_colsize,patches_3dsize);
+for patch_idx = 1:num_patches
+    patches(:,:, patch_idx) = hirise_img(upper_y(patch_idx):upper_y(patch_idx)+patch_size-1, upper_x(patch_idx):upper_x(patch_idx)+patch_size-1);
+    temp2=extractHOGFeatures(patches(:,:,patch_idx),'CellSize',[2 2]);
+    temp4=extractHOGFeatures(patches(:,:,patch_idx),'CellSize',[4 4]);
+    temp8=extractHOGFeatures(patches(:,:,patch_idx),'CellSize',[8 8]);    
+    if patch_idx==1
+       patches_HoG2=zeros(num_patches,size(temp2,2));
+       patches_HoG4=zeros(num_patches,size(temp4,2));
+       patches_HoG8=zeros(num_patches,size(temp8,2));
+       patches_HoG2(patch_idx,:)=temp2;
+       patches_HoG4(patch_idx,:)=temp4;
+       patches_HoG8(patch_idx,:)=temp8;
+    else
+       patches_HoG2(patch_idx,:)=temp2;
+       patches_HoG4(patch_idx,:)=temp4;
+       patches_HoG8(patch_idx,:)=temp8;
+    end
+end
+
+[n, m, samples] = size(patches);
+patches = reshape(permute(patches,[3 2 1]),samples,n*m);
+patches = zscore(patches);
+patches_PCA=pca(patches');
+patches_structure=struct('Patches', {patches}, 'HoG_Patches_2x2', patches_HoG2, 'HoG_Patches_4x4', patches_HoG4,'HoG_Patches_8x8', patches_HoG8, 'Patches_PCA', patches_PCA, 'Upper_X',{upper_x},'Upper_Y', {upper_y}); 
+end
+>>>>>>> origin/master
--- read_reshape_dataset_labeled.m
+++ read_reshape_dataset_labeled.m
@@ -1,8 +1,8 @@
-function [images, labels, feature_array, image_structure] = read_reshape_dataset_labeled(config, pixel_size)
+function image_structure = read_reshape_dataset_labeled(config, pixel_size)
 %In this script, we take all the images and make them the same size
-%as well as we flatten them to input in a classifier
+%as wellas we flatten them to input in a classifier
 %%First we load the data matrix
-folder  = '..\Data\';
+folder  = 'Data\';
 filename = config.data{2};
 strfilename = strsplit(filename, '.');
 strfilename = strfilename{1};
@@ -21,7 +21,6 @@ end
 mat_files = dir(['window_size' num2str(pixel_size) '_*_' strfilename '.mat']); %finds all the files that have widnows of the pixel size
 %%
 %Now we concatenate all of the data files in them
-
 master_data=[];
 master_label = {};
 file_number = size(mat_files,1);
@@ -33,6 +32,7 @@ for file_idx = 1:file_number
     label_file=repmat(file_feat,1,datap);
     master_label = cat(2,master_label, label_file);%concatenate all the labels for this dataset
     master_data = cat(3,data,master_data);
+    clear data
 end
    
 %data = [y_coord(1):y_coord(2)
@@ -41,15 +41,12 @@ end
 image_files = dir([folder, '*.png']);
 number_files = size(image_files,1);
 number_mat_files = size(mat_files,1);
-image_array = [];
 master_im_idx = 1;
-feature_array{2}=[];
-feature_array{4}=[];
-feature_array{6}=[];
-feature_array{8}=[];
-for  file_idx= 1:number_files
-    strfilename = strsplit(image_files(file_idx).name, '.');
-    strfilename = strfilename{1};
+
+% for file_idx= 1:number_files
+file_idx=1;
+     strfilename = strsplit(image_files(file_idx).name, '.');
+     strfilename = strfilename{1};
     %Check if the file image has a correspondet mat file
     for mat_idx = 1:number_mat_files
         str = mat_files(mat_idx).name;
@@ -57,30 +54,43 @@ for  file_idx= 1:number_files
             load(str)
             hirise_img = imread([folder image_files(file_idx).name]);%Load the file image
             hirise_img = double(hirise_img)/255;
+            if mat_idx==1
+                [image_array_rowsize,image_array_colsize]=size(hirise_img(data(2,1,size(data,3)):data(2,2,size(data,3)),data(1,1,size(data,3)):data(1,2,size(data,3))));
+                image_array_3dsize=size(data,3)*number_mat_files*number_files;
+                image_array = zeros(image_array_rowsize,image_array_colsize,image_array_3dsize);
+            end
             for im_idx = 1:size(data,3)
-                test_img = hirise_img(data(2,1,im_idx):data(2,2,im_idx),data(1,1,im_idx):data(1,2,im_idx),:); %extract the imgaages
+                test_img = hirise_img(data(2,1,im_idx):data(2,2,im_idx),data(1,1,im_idx):data(1,2,im_idx)); %extract the imgaages
                 image_array(:,:,master_im_idx) = test_img;
-                for cell_size=2:2:8
-                    features(1,:,master_im_idx)={extractHOGFeatures(test_img,'CellSize',[cell_size cell_size])}; 
-                    feature_array{cell_size}=[feature_array{cell_size} features(1,:,master_im_idx)];
-                    clear features
-                end
+                temp2=extractHOGFeatures(test_img,'CellSize',[2 2]); 
+                temp4=extractHOGFeatures(test_img,'CellSize',[4 4]); 
+                temp8=extractHOGFeatures(test_img,'CellSize',[8 8]); 
+                if im_idx==1
+                    features2=zeros(image_array_3dsize,size(temp2,2));
+                    features4=zeros(image_array_3dsize,size(temp4,2));
+                    features8=zeros(image_array_3dsize,size(temp8,2));
+                    features2(master_im_idx,:)=temp2;
+                    features4(master_im_idx,:)=temp4;
+                    features8(master_im_idx,:)=temp8;
+                else
+                    features2(master_im_idx,:)=temp2;
+                    features4(master_im_idx,:)=temp4;
+                    features8(master_im_idx,:)=temp8;
+                end       
                 master_im_idx = master_im_idx+1;
             end
         end
-    end
-end
-data_folder = '..\Data\';
+     end
+% end
+data_folder = 'data\';
 name = 'all_images_';
 samples = num2str(size(master_label,2));
 savename = [data_folder name, samples,'_', strfilename '.mat'];
 save(savename,'image_array')
-savename_feat = [data_folder name, samples, '_', strfilename '.mat'];
-save(savename_feat,'feature_array')
 [n,m,samples]= size(image_array);
 image_flat = reshape(permute(image_array,[3 2 1]),samples,n*m);
-pcacoeff={pca(image_flat')};
+pcacoeff=pca(image_flat');
 images = image_flat;
 labels = master_label;
-image_structure=struct('Original_Images',{images},'HoG_Features_2x2',{feature_array{2}},'HoG_Features_4x4',{feature_array{4}},'HoG_Features_6x6',{feature_array{6}},'HoG_Features_8x8',{feature_array{8}},'PCA_Coefficients',{pcacoeff},'Labels',{labels});
+image_structure=struct('Original_Images',{images},'HoG_Features_2x2',features2,'HoG_Features_4x4',features4,'HoG_Features_8x8',features8,'PCA_Coefficients',pcacoeff,'Labels',{labels'});
 end
